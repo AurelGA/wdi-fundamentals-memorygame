80f93f51572e099b5084b06c14c50e1bab0cf182
Aurel Zhen Xing - The Immortal Engineer <animasupreme@Aurels-MacBook-Pro.local>
2019-11-12 18:35:32 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,19 @@
 console.log("Up and running!");
 
 
-var cardOne = "king";
 
+var cardOne = "queen";
 var cardTwo = "king";
-
-var cardThree = "queen";
-
+var cardThree = "king";
 var cardFour = "queen";
+
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
+
+
+
+
+
+
+
