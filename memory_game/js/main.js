f100f8526140b3efe561cc66f34a7b4927d90f9f
Aurel Zhen Xing - The Immortal Engineer <animasupreme@Aurels-MacBook-Pro.local>
2019-11-19 12:33:23 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,37 @@
 console.log("Up and running!");
 
+const arrCards = ["queen", "queen", "king", "king"]
+
+const cardsInPlay = []
 
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cardOne = arrCards[0]
+
+var cardTwo = arrCards[2]
+
+var cardThree = arrCards[1]
+
+var cardFour = arrCards[3]
 
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+
+if (cardsInPlay.length === 2) {
+
+if (cardsInPlay[0] == cardsInPlay[1]) {
+
+alert("You found a match"); }
 
 
 
-
-
-
+else {
+ alert("Sorry, try again.");
+}
+}
 
